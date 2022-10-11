213d9fef8aeb90f0cd6ebc2aa355b60118e5121d
sumyataung2004 <sumyataung1584@gmail.com>
2022-10-11 21:55:19 +0630

    1st day

---
+++ ch-1.js
@@ -0,0 +1,28 @@
+//////////////// Challenge - 1
+
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+// Note:
+// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+let classPoints = [12, 23, 34, 45, 56, 67, 78, 89, 90];
+let yourPoints = 9;
+
+classPoints.push(yourPoints);
+
+let totalPoints = classPoints.reduce((accum, currentValue) => {
+  return accum + currentValue;
+}, 0);
+
+let average = totalPoints / classPoints.length;
+
+if (average > yourPoints) {
+  return false;
+} else {
+  return true;
+}
