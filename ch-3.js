86d816c5748bd80cf1d1427a75547224be41845d
sumyataung2004 <sumyataung1584@gmail.com>
2022-10-12 00:03:44 +0630

    2nd day

---
+++ ch-3.js
@@ -0,0 +1,33 @@
+// All of the animals are having a feast! Each animal is bringing one dish.
+
+//There is just one rule: the dish must start and end with the same letters as the animal's name.
+
+//For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+
+// Write a function feast that takes the animal's name and dish as arguments and
+
+// returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+
+// Assume that beast and dish are always lowercase strings, and that each has at least two letters.
+
+//beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let beast = "brown bear";
+let dish = "bear claw";
+
+let firstLetterBeast = beast.charAt(beast.length - 1);
+let lastLetterBeast = beast.slice(0, 1);
+
+let firstLetterdish = dish.charAt(dish.length - 1);
+let lastLetterdish = dish.slice(0, 1);
+
+if (
+  firstLetterBeast === firstLetterdish &&
+  lastLetterBeast === lastLetterdish
+) {
+  //   console.log(true);
+} else {
+  //   console.log(false);
+}
---
+++ ch-4.js
@@ -0,0 +1,9 @@
+// Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language )
+//  that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+
+let numbers = [-52, 56, 30, 29, -54, 0, -110];
+
+let smallest = Math.min.apply(Math, numbers);
+let largest = Math.max.apply(Math, numbers);
+
+// console.log(smallest, largest);
