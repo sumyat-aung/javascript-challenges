e1e5fdea2dea977859d87ba11b3736a724f371a1
sumyataung2004 <sumyataung1584@gmail.com>
2022-11-10 07:37:08 +0630

    day-6

---
+++ ch-18.js
@@ -0,0 +1,27 @@
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function bodyMass(weight, height) {
+  let bmi = weight / (height * 2);
+
+  if (bmi <= 18.5) {
+    console.log("Underweight");
+  } else if (bmi <= 25.0) {
+    console.log("Normal");
+  } else if (bmi <= 30.0) {
+    console.log("Overweight");
+  } else if (bmi > 30) {
+    console.log("Obese");
+  }
+}
+
+bodyMass(231233, 123);
